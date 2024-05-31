211f3aabbdc466ba4956b44d59fd52076916b5d3
Bockkwurst <147827734+Bockkwurst@users.noreply.github.com>
2024-05-31 08:41:54 +0200

    toggle switch wechselt nun die theme modi

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,19 +1,28 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import StartPage from "./pages/StartPage.jsx";
 import ToggleSwitch from "./components/toggle/ToggleSwitch.jsx";
 
 export function App() {
     const [toggled, setToggled] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        if (darkMode){
+            document.body.classList.add("dark-mode");
+        }else{
+            document.body.classList.remove("dark-mode");
+        }
+    }, [darkMode]);
 
     const handleClick = () => {
-        setToggled((s) => !s);
+        setDarkMode(prevMode => !prevMode);
     };
 
     return (
         <BrowserRouter>
             <div>
-                <ToggleSwitch toggled={toggled} onClick={handleClick}/>
+                <ToggleSwitch toggled={darkMode} onClick={handleClick}/>
                 <Routes>
                     <Route path="/" element={<StartPage/>}/>
                 </Routes>
