404cd2c83e7bfb44082bdab666274d415380bf53
Bockkwurst <kremer.1991@web.de>
2024-06-03 11:50:29 +0200

    startpage ready

--- todolist/src/components/toggle/ToggleSwitch.jsx
+++ todolist/src/components/toggle/ToggleSwitch.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import styles from "./toggleswitch.module.css";
 
-export default function ToggleSwitch({toggled, onClick}){
+export default function ToggleSwitch({toggled, onClick}) {
 
 
-    return(
+    return (
         <div onClick={onClick} className={`${styles.toggle} ${toggled ? styles.night : ""}`}>
-<div className={styles.notch}>
-    <div className={styles.crater1}/>
-    <div className={styles.crater2}/>
-</div>
-    <div className={`${styles.shape} ${styles.sm1}`}/>
-    <div className={`${styles.shape} ${styles.sm2}`}/>
-    <div className={`${styles.shape} ${styles.md}`}/>
-    <div className={`${styles.shape} ${styles.lg}`}/>
+            <div className={styles.notch}>
+                <div className={styles.crater1}/>
+                <div className={styles.crater2}/>
+            </div>
+            <div className={`${styles.shape} ${styles.sm1}`}/>
+            <div className={`${styles.shape} ${styles.sm2}`}/>
+            <div className={`${styles.shape} ${styles.md}`}/>
+            <div className={`${styles.shape} ${styles.lg}`}/>
 
         </div>
     )
