024191e74962e8386ba5795b5cba6e0a04c508fe
Bockkwurst <kremer.1991@web.de>
2024-06-04 11:58:36 +0200

    login sollte funktionieren

--- todolist/src/components/loginForm/LoginForm.jsx
+++ todolist/src/components/loginForm/LoginForm.jsx
@@ -1,23 +1,51 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import './loginform.css'
 import axios from "axios";
 import {Link, useNavigate} from "react-router-dom";
 import DefaultButton from "../../components/defaultButton/DefaultButton.jsx";
-import { FaEye } from "react-icons/fa";
+import { FaEye, FaEyeSlash} from "react-icons/fa";
+import AuthProvider, {useAuth} from "../../utils/AuthProvider.jsx";
 
 const LoginForm = ({darkMode}) => {
 
     const loginPageContainerClass = darkMode ? "login-page-container dark-mode" : "login-page-container light-mode";
-    const title1Class = darkMode ? "title-1 dark-mode" : "title-1 light-mode";
-    const title2Class = darkMode ? "title-2 dark-mode" : "title-2 light-mode";
-    const title3Class = darkMode ? "title-3 dark-mode" : "title-3 light-mode";
     const logoSrc = darkMode ? "/images/logo-hoch-weiss.png" : "/images/logo-hoch-dunkel.png";
+
+    const navigate = useNavigate();
+
+    const {setToken} = useAuth();
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [buttonClicked, setButtonClicked] = useState(false);
 
     const [showPassword, setShowPassword] = useState(false);
 
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
     };
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:3000/user/atuhenticate', {login, password});
+            if (response.data && response.data.token){
+                setToken(response.data.token);
+                navigate('/home', {replace: true});
+            }else{
+                setError('Login failed');
+            }
+        }catch (error){
+            console.error('Login error:', error);
+            setError('Login failed');
+        }
+    }
+
+    const handleButtonClick = (event) => {
+        event.preventDefault();
+        handleLogin();
+        setButtonClicked(true);
+    }
 
     return (
         <div className={loginPageContainerClass}>
@@ -27,18 +55,21 @@ const LoginForm = ({darkMode}) => {
             <div className="form-container">
                 <form>
                     <label className="label">Benutzername:</label>
-                    <input type="text" placeholder="Benutzername" className="input"/>
+                    <input type="text" placeholder="Benutzername" value={login} onChange={e => setLogin(e.target.value)} className="input"/>
                     <label className="label">Passwort:</label>
-                    <input type={showPassword ? "text" : "password"} placeholder="Passwort" className="input"/>
+                    <input type={showPassword ? "text" : "password"} placeholder="Passwort" value={password} onChange={e => setPassword(e.target.value)} className="input"/>
                     <button type="button" onClick={toggleShowPassword}>
-                        {showPassword ? <FaEye/> : <FaEye/>}
+                        {showPassword ? <FaEye/> : <FaEyeSlash/>}
                     </button>
                 </form>
             </div>
             <div className="button-container">
                 <Link to="/home">
-                    <DefaultButton buttonText="Login"/>
+                    <DefaultButton buttonText="Login" onClick={(event) => handleButtonClick(event)}/>
                 </Link>
+            </div>
+            <div className="error-message">
+                <label className="error">{error}</label>
             </div>
         </div>
     )
